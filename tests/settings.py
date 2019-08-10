c54cd586ec4b7e21c0a1cc4af04b940b7be0fa29
Grégoire ROCHER <gregoire@emencia.com>
2019-08-10 21:48:45 +0200

    Tests

--- tests/settings.py
+++ tests/settings.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
 SECRET_KEY = '4'
+INSTALLED_APPS = [
+    'django_exec',
+]
--- tests/test_exec.py
+++ tests/test_exec.py
@@ -7,99 +7,82 @@ import os.path
 from django.utils import six
 
 import importlib
-# exec is a py2 syntax error
-exec_ = importlib.import_module('django_exec.management.commands.exec')
-Command = exec_.Command
+from django.core.management import call_command, CommandError
 
 
 def test_exec_help():
-    c = Command()
-    with pytest.raises(SystemExit) as e:
-        c.run_from_argv(['./manage.py', 'exec'])
-    assert e.value.args == (2, )
+    with pytest.raises(CommandError) as e:
+        call_command('exec')
 
 
 @pytest.fixture
 def stdout():
-    if six.PY2:
-        return io.BytesIO()
-    else:
-        return io.StringIO()
+    return io.StringIO()
 
 
-@pytest.fixture
-def command(stdout):
-    stdin = io.StringIO(u'1 + 1')
-    return Command(stdout=stdout, stdin=stdin)
-
-
-def test_exec(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', '1 + 1'])
+def test_exec(stdout):
+    call_command('exec', '1 + 1', stdout=stdout)
     assert stdout.getvalue() == '>>> 1 + 1\n    2\n'
 
 
-def test_exec_stdin(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', '-'])
+def test_exec_stdin(monkeypatch, stdout):
+    monkeypatch.setattr('sys.stdin', io.StringIO('1 + 1'))
+    call_command('exec', '-', stdout=stdout)
     assert stdout.getvalue() == '>>> 1 + 1\n    2\n'
 
 
-def test_exec_stdin_bytes(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', '-'])
-    assert stdout.getvalue() == '>>> 1 + 1\n    2\n'
-
-
-def test_exec_file(command, stdout):
+def test_exec_file(stdout):
     path = os.path.join(os.path.dirname(__file__), 'data', 'input')
-    command.run_from_argv(['./manage.py', 'exec', path])
+    call_command('exec', path, stdout=stdout)
     assert stdout.getvalue() == '>>> 1 + 1\n    2\n'
 
 
-def test_exec_file_multiline(command, stdout):
+def test_exec_file_multiline(stdout):
     path = os.path.join(os.path.dirname(__file__), 'data', 'multiline')
-    command.run_from_argv(['./manage.py', 'exec', path])
+    call_command('exec', path, stdout=stdout)
     assert stdout.getvalue() == '>>> a = 1\n    a: 1\n>>> 1 + 1\n    2\n'
 
 
-def test_exec_assign(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', 'a = 1'])
+def test_exec_assign(stdout):
+    call_command('exec', 'a = 1', stdout=stdout)
     assert stdout.getvalue() == '>>> a = 1\n    a: 1\n'
 
 
-def test_exec_print(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', '__import__("sys").stdout.write("1\\n")'])
+def test_exec_print(stdout):
+    call_command( 'exec', '__import__("sys").stdout.write("1\\n")', stdout=stdout)
     assert stdout.getvalue() == '>>> __import__("sys").stdout.write("1\\n")\n    None\n'
 
 
-def test_exec_import(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', 'from os import SEEK_CUR'])
+def test_exec_import(stdout):
+    call_command('exec', 'from os import SEEK_CUR', stdout=stdout)
     assert stdout.getvalue() == '>>> from os import SEEK_CUR\n    SEEK_CUR: 1\n'
 
 
-def test_exec_keep_context(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', 'a = 1;a += 1'])
+def test_exec_keep_context(stdout):
+    call_command('exec', 'a = 1;a += 1', stdout=stdout)
     assert stdout.getvalue() == '>>> a = 1\n    a: 1\n>>> a += 1\n    a: 1 -> 2\n'
 
 
-def test_exception(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', 'b'])
+def test_exception(stdout):
+    call_command('exec', 'b', stdout=stdout)
     output = stdout.getvalue()
     assert '''NameError: name 'b' is not defined''' in output
     assert '''File "b", line 1, in <module>''' in output
 
 
-def test_syntax_error(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', 'b -'])
+def test_syntax_error(stdout):
+    call_command('exec', 'b -', stdout=stdout)
     output = stdout.getvalue()
     assert '''SyntaxError: invalid syntax''' in output
 
 
-def test_print_dict(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', 'dict(a=1, b=2)'])
+def test_print_dict(stdout):
+    call_command('exec', 'dict(a=1, b=2)', stdout=stdout)
     if sys.version_info.major == 3 and sys.version_info.minor < 6:
         pytest.xfail('Inconsistent dict hashing until python 3.6')
     assert stdout.getvalue() == ">>> dict(a=1, b=2)\n    {\n        'a': 1,\n        'b': 2,\n    }\n"
 
 
-def test_class_decl(command, stdout):
-    command.run_from_argv(['./manage.py', 'exec', 'class A(object):pass'])
+def test_class_decl(stdout):
+    call_command('exec', 'class A(object):pass', stdout=stdout)
     assert stdout.getvalue() == ">>> class A(object):pass\n    A: <class 'A'>\n"
